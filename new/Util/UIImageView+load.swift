27e61b06641fc18b65c86027d222242b2553194d
ej.lim <ej.lim@lge.com>
2024-04-01 21:31:06 +0900

    Utils (UIButton+setBackground, UIImageView+load) 추가

---
+++ new/Util/UIImageView+load.swift
@@ -0,0 +1,22 @@
+//
+//  UIImage+load.swift
+//  Search
+//
+//  Created by 임은지 on 4/1/24.
+//
+
+import UIKit
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
