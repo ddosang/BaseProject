e87e2b5dba7958785fb5b1ede38a67286751643f
임은지 <ej.lim@lge.com>
2024-04-03 14:19:59 +0900

    add snapkit and then by using SPM

--- BaseProj/ViewController.swift
+++ BaseProj/ViewController.swift
@@ -8,13 +8,29 @@
 import UIKit
 
 class ViewController: BaseViewController {
+    
+    private let nameButton: UIButton = .init().then { b in
+        b.backgroundColor = .systemBlue
+        b.setTitle("asdf", for: .normal)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .blue
     }
 
+    override func setup() {
+        super.setup()
+        view.addSubview(nameButton)
+    }
+    
+    override func bindConstraints() {
+        super.bindConstraints()
+        
+        nameButton.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
 
 }
 
